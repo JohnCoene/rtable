ffc17421ee261bb3a9c1f6820fda4ae61e9ddb3e
John Coene <jcoenep@gmail.com>
2019-04-12 10:25:44 +0200

    rm shiny

--- R/zzz.R
+++ R/zzz.R
@@ -3,14 +3,4 @@
     "Welcome to rtable!\n\n",
     "Examples: rtable.john-coene.com"
   )
-  
-  shiny::registerInputHandler("echarts4rParse", function(data, ...) {
-    jsonlite::fromJSON(jsonlite::toJSON(data, auto_unbox = TRUE))
-  }, force = TRUE)
-  
-  options(
-    "ECHARTS4R_THEME" = NULL,
-    "ECHARTS4R_FONT_FAMILY" = NULL
-  )
-  
 }
